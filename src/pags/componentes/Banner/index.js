0bd2f25d78a43b8e9f7afaaa9bb57c50c3d20be3
Jeffmx <jeffersonmx6@gmail.com>
2024-12-19 12:00:59 -0300

    hm

--- src/pags/componentes/Banner/index.js
+++ src/pags/componentes/Banner/index.js
@@ -1,7 +1,16 @@
+import styles from './Banner.module.css';
+import circuloColorido from 'assets/circulo_colorido.png';
+
 export default function Banner() {
-    return(
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <div>
+        <h1 className={styles.titulo}>Olá, mundo!</h1>
+        <p className={styles.paragrafo}>Boas vindas ao meu espaço pessoal! Eu sou Antônio Evaldo, instrutor de Front-end da Alura. Aqui compartilho vários conhecimentos, espero que aprenda algo novo</p>
+      </div>
+      <div>
+        <img src={circuloColorido} />
+      </div>
+    </div>
+  )
 }
--- src/pags/inicio/index.js
+++ src/pags/inicio/index.js
@@ -1,7 +1,9 @@
+import Banner from "../componentes/Banner";
+
 export default function Inicio() {
     return(
         <main>
-            
+            <Banner/>
             <h1>mano?</h1>
         </main>
     )
